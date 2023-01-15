03e62df95ad557cc62005777898d3534510001d3
mr-shitij <21.shitijagrawal@gmail.com>
2023-01-15 23:10:50 +0530

    New

--- C_language/csv/shell/shell.c
+++ C_language/csv/shell/shell.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
 int main() {
-    char command[30] = "exit";
+    // execl("/bin/ls", "ls", "-l", NULL);
+    char command[1024] = "exit";
     do {
         printf("prompt>");
         scanf("%s", command);
         int pid = fork();
         if(pid == 0) {
+            char arguments[1024];
+
             execl(command, command, NULL);
         } else {
             wait(0);
